ba6db3a04ef0e1e0d29e92adcc07d9e0fc5a5035
Hideon0507 <96288480+Hideon0507@users.noreply.github.com>
2022-12-02 10:00:18 +0100

    Update OrdiAI.py

--- OrdiAI.py
+++ OrdiAI.py
@@ -2,7 +2,7 @@
 from random import randint
 from BaseAI import BaseAI
 
-class OrdiAI(BaseAI):
+class OrdiAI():
     def getMove(self, grid):
         cells = grid.getCellVide()
         return cells[randint(0, len(cells) - 1)] if cells else None
